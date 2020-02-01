3b14e549cde0588f1bc6434a32498055a8623e3e
roy <roy@10.5.55.13>
2020-02-01 19:25:41 +0700

    Update new test code example

--- test.m
+++ test.m
@@ -4,16 +4,20 @@ import ExeQu.Gates.*
 import ExeQu.Utils.*
 
 
-circuit = Circuit(3, 2)
+circuit = Circuit(2, 3)
 
 circuit.x(1)
-circuit.cnot(2, 1)
-circuit.cy(1, 2)
-circuit.ccnot(1, 2, 3);
+circuit.h(2)
+circuit.measure(1, 1)
+circuit.measure(2, 2)
+% circuit.cnot(2, 1)
+% circuit.cy(1, 2)
+% circuit.ccnot(1, 2, 3)
 %circuit.unitary([0 1; 1 0], 2)
 
 
-circuit.draw()
-
-circuit.quantumRegister
+%circuit.draw()
 circuit.peekOperations()
+result = circuit.execute();
+result.getCount();
+result.plotHistogram();
