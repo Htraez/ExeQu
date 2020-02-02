9e1c1912661665bb333b54ce6f604fff34d99ab0
roy <roy@10.5.55.13>
2020-02-02 15:57:06 +0700

    Update example code

--- test.m
+++ test.m
@@ -4,19 +4,26 @@ import ExeQu.Gates.*
 import ExeQu.Utils.*
 
 
-circuit = Circuit(2, 3)
+circuit = Circuit(3, 3)
 
-circuit.x(1)
-circuit.h(2)
+circuit.h(1)
+circuit.x(2)
+circuit.cnot(1, 2)
+U = circuit.unitary([0 1; 1 0]);
+circuit.controlledU(U,[1, 2], 3);
+circuit.controlledU(@PauliX,[1, 2], 3);
+circuit.controlledU('x',[1, 2], 3);
 circuit.measure(1, 1)
 circuit.measure(2, 2)
+circuit.measure(3, 3)
 % circuit.cnot(2, 1)
-% circuit.cy(1, 2)
-% circuit.ccnot(1, 2, 3)
-%circuit.unitary([0 1; 1 0], 2)
+circuit.cy(1, 2)
+circuit.ccnot(1, 2, 3)
+circuit.unitary([0 1; 1 0], 2)
 
 circuit.draw();
 circuit.peekOperations()
-result = circuit.execute();
+result = circuit.execute(2000);
+result
 result.getCount();
 result.plotHistogram();
