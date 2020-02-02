c86d031a741f7ec5d364cc05a988f5f2316128ce
SMooTHiE <jojojj2012@hotmail.com>
2020-02-02 19:20:22 +0700

    update controlled-controlled-y controlled-controlled-u multiple controlled-u multiple controlled-y multiple controlled-z

--- +ExeQu/+Utils/@Visualization/plotOperation.m
+++ +ExeQu/+Utils/@Visualization/plotOperation.m
@@ -79,16 +79,60 @@ function plotOperation(op)
         % crate gate
         
         if check=="controlled-u"
-            check="u";
+           check="u";
+        elseif check=="cy"
+           check="y";
+        elseif check=="cz"
+           check="z";
         end
-        text(start_x(1)+0.3,start_y+0.5-(2*distance),upper(check));
+        text(start_x(1)+0.4,start_y+0.5-(2*distance),upper(check));
+        % debug text
+        
+    elseif check=="controlled-controlled-y" || check=="controlled-controlled-u" || check=="multiple controlled-u" || check=="multiple controlled-y" || check=="multiple controlled-z"
+        L = max(op.associatedQubit)-min(op.associatedQubit);
+        n_element=[4 4 4 4 4];
+        pos_x = 3 + (margin_line_x*n_element(op.associatedQubit)) + n_element(op.associatedQubit);
+        pos_y = -2*min(op.associatedQubit);
+        % pos_x,pos_y is position line
+        
+        yline=[pos_y pos_y-(2*L)];
+        line([pos_x(1) pos_x(1)],yline);                        %//edit pos_x(1-2)
+        
+        start_x = pos_x-0.5;
+        % start_x is left-bottom angle of rectangle
+        
+        for a = 1:1:length(op.associatedQubit)
+            r = 0.15;
+            c = [pos_x(1) -(op.associatedQubit(a)*2)];
+            % radius and center of small circle 
+            % position of small circle.It means ctrl
+        
+            pos = [c-r 2*r 2*r];
+            rectangle('Position',pos,'Curvature',[1 1], 'FaceColor', [0.3010 0.7450 0.9330], 'Edgecolor','none')
+            % create small circle
+            if a == length(op.associatedQubit)
+            	rectangle('Position',[start_x(1) -(2*op.associatedQubit(a))-0.5 1 1],'FaceColor',[1 1 1]);
+                axis([0 inf -inf 0]);
+                % crate gate
+                if check=="controlled-controlled-u"
+                    check="u";
+                elseif check=="controlled-controlled-y"
+                    check="y";
+                elseif check=="multiple controlled-u"
+                    check="u";
+                elseif check=="multiple controlled-y"
+                    check="y";
+                elseif check=="multiple controlled-z"
+                    check="z";
+                end
+            end
+        end
+        text(start_x(1)+0.4,-(2*op.associatedQubit(a)),upper(check));
         % debug text
         
     elseif check=="toffoli" || check=="multiple control toffoli"
-        %L=length(op.associatedQubit);
         L = max(op.associatedQubit)-min(op.associatedQubit);
         n_element=[3 3 3 3 3];
-        %distance = op.associatedQubit(2)-op.associatedQubit(1);
         pos_x = 3 + (margin_line_x*n_element(op.associatedQubit)) + n_element(op.associatedQubit);
         pos_y = -2*min(op.associatedQubit);
         % pos_x,pos_y is position ccnot-line
--- test.m
+++ test.m
@@ -9,9 +9,9 @@ circuit.h(1)
 circuit.x(2)
 circuit.cnot(1, 3)
 U = circuit.unitary([0 1; 1 0]);
-circuit.controlledU(U,[1, 2], 3);
-%circuit.controlledU(@PauliX,[1, 3], 5);
-circuit.controlledU('x',[1, 4], 2);
+circuit.controlledU(U,[3,4], 5);
+circuit.controlledU(@PauliX,[1,2, 5], 3);
+%circuit.controlledU('x',[1, 4], 2);
 %circuit.measure(1, 1)
 %circuit.measure(2, 2)
 %circuit.measure(3, 3)
