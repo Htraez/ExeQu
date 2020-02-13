273f5e8ddf76fe42854fb51de5d43ee0829f22ab
roy <roy@localhost>
2020-02-13 17:26:19 +0700

    - Add new tutorial

--- sandbox.m
+++ sandbox.m
@@ -3,38 +3,10 @@ import ExeQu.CircuitComposer.*;
 
 
 
-circuit = Circuit(5, 3);
-
-circuit.h(1)
-circuit.x(2)
-circuit.h(1)
-circuit.x(2)
-circuit.h(1)
-circuit.x(2)
-circuit.h(1)
-circuit.x(2)
-circuit.h(1)
-circuit.x(2)
-circuit.h(1)
-circuit.x(2)
-circuit.cz(5, 3)
-circuit.cnot(4, 2)
-circuit.cnot(4, 5)
-U = Unitary([0 1; 1 0]);
-circuit.controlledU(U,[3,4], 5);
-
-circuit.cy(4, 1)
-circuit.controlledU(@PauliX,[1,2,3], 5);
-circuit.controlledU('x',[1, 4], 2);
-circuit.measure(1, 1)
-%circuit.measure(2, 2)
-%circuit.measure(3, 3)
-%% circuit.cnot(2, 1)
-%circuit.ccnot(2, 5, 3)
-circuit.unitary([0 1; 1 0], 2);
-
+circuit = Circuit(1, 1);
+circuit.measure(1, 1);
 circuit.draw();
-% circuit.peekOperations()
+circuit.peekOperations()
 %result = circuit.execute(2000);
 %result;
 %result.getCount();
