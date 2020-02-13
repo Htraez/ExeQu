2307812a6e31072b22a4cf669a429adfd9010496
roy <roy@localhost>
2020-02-13 16:15:10 +0700

    - Circuit.unitary is used to add unitary to circuit only for ease of use. Independent unitary can be initialized by Unitary() in Gates

--- +ExeQu/+CircuitComposer/@Circuit/unitary.m
+++ +ExeQu/+CircuitComposer/@Circuit/unitary.m
@@ -1,17 +1,10 @@
-function operation = unitary(self, operator, varargin)
+function operation = unitary(self, operator, target)
     import ExeQu.Gates.*;
     registerSize = self.quantumRegister.getSize();
-    if nargin < 3 
-        % In case user just want 2x2 unitary without caring about
-        % register size
-        target = 1;
-        registerSize = 1;
-    else 
-        target = varargin{1};
-    end
 
     U = Unitary(operator, registerSize, target);
     operation.unitaryOperation = U;
     operation.label = U.label;
     operation.associatedQubit = [target];
+    self.add(operation);
 end
