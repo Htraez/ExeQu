9f9e5efd3e293c2efa056d3006a19d5621ca0f16
Nawakanok <guitar_sk555@hotmail.com>
2020-02-19 14:18:27 +0700

    add check syntax openqasm, include, qreg, creg, comment

--- +ExeQu/+Transpiler/Transpiler.m
+++ +ExeQu/+Transpiler/Transpiler.m
@@ -1,62 +1,110 @@
 classdef Transpiler
+    properties
+        qregName = [];
+        cregName = [];
+    end
     
     methods
         function convert(self,inFileName, outFileName)
             text = self.readFile(inFileName);
             allCmd = self.seperateCommand(text);
-            correctCmd = self.checkSyntax(allCmd);
-            self.writeFile(correctCmd, outFileName);
+            self.writeFile(allCmd, outFileName);
         end
     end
     
     methods(Access=private)
-        function text = readFile(self,inFileName)
+        function text = readFile(~,inFileName)
             fileID = fopen(inFileName,'r');
             formatSpec = '%c';
             text = fscanf(fileID, formatSpec);
         end
         
-        function allCmd = seperateCommand(self,text)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
+        function allCmd = seperateCommand(~,text)
             text = strrep(text,'{',';begin;');
             text = strrep(text,'}',';end;');
             allCmd = strsplit(text,{';','\n'});
             allCmd = strtrim(allCmd);
         end
         
-        function correctCmd = checkSyntax(self,allCmd)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            correctCmd = cell(size(allcmd));
-            for k=1:length(allCmd)
-                cmd = strsplit(allCmd{k},' ');
-                if length(cmd)==2
-                    if strcmp(cmd{1},'qreg')
-                        disp('qreg');
-                    elseif strcmp(cmd{1},'creg')
-                        disp('creg'); 
-                    elseif strcmp(cmd{1},'OPENQASM') && strcmp(cmd{1},'2.0')
-                        disp('OPENQASM');
-                    elseif strcmp(cmd{1},'measure')
-                        disp('measure');
-                    elseif strcmp(cmd{1},'cx')
-                        disp('cx');
-                    elseif strcmp(cmd{1},'include')
-                        disp('include');
-                    elseif strcmp(cmd{1},'reset')
-                        disp('reset');
-                    elseif startsWith(cmd{1},'//')
-                        disp('comment');
+        function correctCmd = checkQASMSyntax(self,cmd)
+            
+            correctCmd = {};
+            
+            if startsWith(cmd,'OPENQASM ')
+                tmp = strsplit(cmd,{' '});
+                tmp = strtrim(tmp);
+                if( strcmp(tmp{1},'OPENQASM ') && strcmp(tmp{2},'2.0') )
+                    correctCmd = tmp;
+                end
+            elseif startsWith(cmd,'include ')
+                tmp = strsplit(cmd,{'"'});
+                tmp = strtrim(tmp);
+                if( strcmp(tmp{1},'include') && startsWith(tmp{2},'"') && endsWith(tmp{2},'"') )
+                    correctCmd = tmp;
+                end
+            elseif startsWith(cmd,'qreg ')
+                tmp = strsplit(cmd,' ');
+                tmp = strtrim(tmp);
+                if strcmp(tmp{1},'qreg')
+                    qreg = strrep(tmp{2},'[',' [ ');
+                    qreg = strrep(qreg,']',' ] ');
+                    qreg = strsplit(qreg,{'[',']'});
+                    qreg = strtrim(qreg);
+                    if strcmp(qreg{2},'[') && strcmp(qreg{4},']')
+                        pattern = {'1','2','3','4','5','6','7','8','9','0'};
+                        if ( ~isnan(str2double(qreg{3}))) && ( ~startsWith(qreg{1},pattern) )
+                            qregNo = length(self.qregName);
+                            self.qregName{qregNo+1} = qreg{1};
+                            tmp = strrep(cmd,'[',' [ ');
+                            tmp = strrep(tmp,']',' ] ');
+                            tmp = strsplit(tmp,{' ','[',']'});
+                            tmp = strtrim(tmp);
+                            correctCmd = tmp;
+                        end
                     end
                 end
-                correctCmd{k} = allCmd{k};
+            elseif startsWith(cmd,'creg ')
+                tmp = strsplit(cmd,' ');
+                tmp = strtrim(tmp);
+                if strcmp(tmp{1},'creg')
+                    creg = strrep(tmp{2},'[',' [ ');
+                    creg = strrep(creg,']',' ] ');
+                    creg = strsplit(creg,{'[',']'});
+                    creg = strtrim(creg);
+                    if strcmp(creg{2},'[') && strcmp(creg{4},']')
+                        pattern = {'1','2','3','4','5','6','7','8','9','0'};
+                        if ( ~isnan(str2double(creg{3}))) && ( ~startsWith(creg{1},pattern) )
+                            cregNo = length(self.cregName);
+                            self.cregName{cregNo+1} = creg{1};
+                            tmp = strrep(cmd,'[',' [ ');
+                            tmp = strrep(tmp,']',' ] ');
+                            tmp = strsplit(tmp,{' ','[',']'});
+                            tmp = strtrim(tmp);
+                            correctCmd = tmp;
+                        end
+                    end
+                end
+            elseif startsWith(cmd,'cx ')
+                tmp = strrep(cmd,',',' , ');
+                tmp = strsplit(tmp,' ');
+                tmp = strtrim(tmp);
+                if strcmp(tmp{1},'cx') && strcmp(tmp{3},',')
+                
+                end
+            elseif startsWith(cmd,'measure ')
+                disp('measure');
+            elseif startsWith(cmd,'//')
+                correctCmd = cmd;
+                return;
             end
+            
+            if isempty(correctCmd)
+                disp('Bad line of code skip...')
+            end
+            
         end
         
-        function writeFile(self,textToWirte, outFileName)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
+        function writeFile(~,textToWirte, outFileName)
             fileID = fopen(outFileName,'w');
             for k=1:length(textToWirte)
                 fprintf(fileID,'%s\n',textToWirte{k});
@@ -64,12 +112,14 @@ classdef Transpiler
         end
         
         function translate(self,codes)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
             for k=1:length(codes)
-                disp(codes{k})
+                correctCmd = checkQASMSyntax(code);
+                if isempty(correctCmd)
+                    
+                end
             end
         end
+        
     end
     
 end
