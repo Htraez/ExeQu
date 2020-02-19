207d8db91d14068dc8f91fa68494cb086ad08d2b
roy <roy@localhost>
2020-02-19 14:20:26 +0700

    - Add new gates (Not tested)

--- +ExeQu/+Gates/Sdag.m
+++ +ExeQu/+Gates/Sdag.m
@@ -3,10 +3,22 @@ classdef Sdag < ExeQu.Gates.Unitary
         operator = [1 0; 0 -1i];
     end
     methods 
-        function obj = Sdag(registerLength, target)
+        function obj = Sdag(varargin)
             % S-dagger => Conjugate Transposed form of S gate
+            % obj = Sdag()
+            % obj = Sdag(registerLength, target)
             import ExeQu.Utils.Maths.*;
             import ExeQu.Gates.*;
+            
+            % Verify parameter
+            if nargin == 2
+                registerLength = varargin{1};
+                target = varargin{2};
+            elseif nargin == 0
+                registerLength = 1;
+                target = [];
+            end
+            
             obj = obj@ExeQu.Gates.Unitary(Sdag.operator, registerLength, target);
             obj.label = 'Sdag';
         end
