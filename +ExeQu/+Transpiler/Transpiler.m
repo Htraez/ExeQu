ead1870336a76194c52995b35b4da33092a54625
Nawakanok <guitar_sk555@hotmail.com>
2020-02-20 22:51:03 +0700

    complete syntax for command OPENQASM 2.0, include, qreg, creg, cx, measure, and comment. some command can convert to ExeQu

--- +ExeQu/+Transpiler/Transpiler.m
+++ +ExeQu/+Transpiler/Transpiler.m
@@ -1,46 +1,56 @@
 classdef Transpiler
     properties
+        text;
+        allCmd;
+        correctCmd;
+        
         qregName = [];
         cregName = [];
+        
+        noOfQreg = [];
+        noOfCreg = [];
+
+        result = []
+        
     end
     
     methods
-        function convert(self,inFileName, outFileName)
-            text = self.readFile(inFileName);
-            allCmd = self.seperateCommand(text);
-            self.writeFile(allCmd, outFileName);
+        function self = convert(self,inFileName, outFileName)
+            self = self.readFile(inFileName);
+            self = self.seperateCommand(self.text);
+            self = self.translate(self.allCmd);
+            self.writeFile(self.result,outFileName);
         end
     end
     
     methods(Access=private)
-        function text = readFile(~,inFileName)
+        function self = readFile(self,inFileName)
             fileID = fopen(inFileName,'r');
             formatSpec = '%c';
-            text = fscanf(fileID, formatSpec);
+            self.text = fscanf(fileID, formatSpec);
         end
         
-        function allCmd = seperateCommand(~,text)
+        function self = seperateCommand(self,text)
             text = strrep(text,'{',';begin;');
             text = strrep(text,'}',';end;');
-            allCmd = strsplit(text,{';','\n'});
-            allCmd = strtrim(allCmd);
+            self.allCmd = strsplit(text,{';','\n'});
+            self.allCmd = strtrim(self.allCmd);
         end
         
-        function correctCmd = checkQASMSyntax(self,cmd)
+        function self = checkQASMSyntax(self,cmd)
             
-            correctCmd = {};
-            
+            self.correctCmd = {};
             if startsWith(cmd,'OPENQASM ')
-                tmp = strsplit(cmd,{' '});
+                tmp = strsplit(cmd,' ');
                 tmp = strtrim(tmp);
-                if( strcmp(tmp{1},'OPENQASM ') && strcmp(tmp{2},'2.0') )
-                    correctCmd = tmp;
+                if( strcmp(tmp{1},'OPENQASM') && strcmp(tmp{2},'2.0') )
+                    self.correctCmd = tmp;
                 end
             elseif startsWith(cmd,'include ')
-                tmp = strsplit(cmd,{'"'});
+                tmp = strsplit(cmd,' ');
                 tmp = strtrim(tmp);
                 if( strcmp(tmp{1},'include') && startsWith(tmp{2},'"') && endsWith(tmp{2},'"') )
-                    correctCmd = tmp;
+                    self.correctCmd = tmp;
                 end
             elseif startsWith(cmd,'qreg ')
                 tmp = strsplit(cmd,' ');
@@ -48,18 +58,19 @@ classdef Transpiler
                 if strcmp(tmp{1},'qreg')
                     qreg = strrep(tmp{2},'[',' [ ');
                     qreg = strrep(qreg,']',' ] ');
-                    qreg = strsplit(qreg,{'[',']'});
+                    qreg = strsplit(qreg,' ');
                     qreg = strtrim(qreg);
                     if strcmp(qreg{2},'[') && strcmp(qreg{4},']')
                         pattern = {'1','2','3','4','5','6','7','8','9','0'};
                         if ( ~isnan(str2double(qreg{3}))) && ( ~startsWith(qreg{1},pattern) )
                             qregNo = length(self.qregName);
                             self.qregName{qregNo+1} = qreg{1};
+                            self.noOfQreg{qregNo+1} = qreg{3};
                             tmp = strrep(cmd,'[',' [ ');
                             tmp = strrep(tmp,']',' ] ');
                             tmp = strsplit(tmp,{' ','[',']'});
                             tmp = strtrim(tmp);
-                            correctCmd = tmp;
+                            self.correctCmd = tmp;
                         end
                     end
                 end
@@ -69,18 +80,19 @@ classdef Transpiler
                 if strcmp(tmp{1},'creg')
                     creg = strrep(tmp{2},'[',' [ ');
                     creg = strrep(creg,']',' ] ');
-                    creg = strsplit(creg,{'[',']'});
+                    creg = strsplit(creg,' ');
                     creg = strtrim(creg);
                     if strcmp(creg{2},'[') && strcmp(creg{4},']')
                         pattern = {'1','2','3','4','5','6','7','8','9','0'};
                         if ( ~isnan(str2double(creg{3}))) && ( ~startsWith(creg{1},pattern) )
                             cregNo = length(self.cregName);
                             self.cregName{cregNo+1} = creg{1};
+                            self.noOfCreg{cregNo+1} = creg{3};
                             tmp = strrep(cmd,'[',' [ ');
                             tmp = strrep(tmp,']',' ] ');
                             tmp = strsplit(tmp,{' ','[',']'});
                             tmp = strtrim(tmp);
-                            correctCmd = tmp;
+                            self.correctCmd = tmp;
                         end
                     end
                 end
@@ -89,16 +101,127 @@ classdef Transpiler
                 tmp = strsplit(tmp,' ');
                 tmp = strtrim(tmp);
                 if strcmp(tmp{1},'cx') && strcmp(tmp{3},',')
-                
+                    
+                    if ( ~isempty(strfind(tmp{2},'[')) ) && ( ~isempty(strfind(tmp{2},']')) ) && ( ~isempty(strfind(tmp{4},'[')) ) && ( ~isempty(strfind(tmp{4},']')) )
+                        keep=0;
+                        qb = strrep(tmp{2},'[',' [ ');
+                        qb = strrep(qb,']',' ] ');
+                        qb = strsplit(qb,' ');
+                        qb = strtrim(qb);
+                        if strcmp(qb{2},'[') && strcmp(qb{4},']')
+                            for i=1:length(self.qregName)
+                                if (strcmp(qb{1},self.qregName{i}))
+                                    if ( ~isnan(str2double(qb{3})) )
+                                        keep = keep+1;
+                                    end
+                                end
+                            end
+                        end
+                        qb = strrep(tmp{4},'[',' [ ');
+                        qb = strrep(qb,']',' ] ');
+                        qb = strsplit(qb,' ');
+                        qb = strtrim(qb);
+                        if strcmp(qb{2},'[') && strcmp(qb{4},']')
+                            for i=1:length(self.qregName)
+                                if (strcmp(qb{1},self.qregName{i}))
+                                    if ( ~isnan(str2double(qb{3})) )
+                                        keep = keep+1;
+                                    end
+                                end
+                            end
+                        end
+                        if(keep==2)
+                            tmp = strrep(cmd,',',' , ');
+                            tmp = strrep(tmp,'[',' [ ');
+                            tmp = strrep(tmp,']',' ] ');
+                            tmp = strsplit(tmp,{',',' ','[',']'});
+                            tmp = strtrim(tmp);
+                            self.correctCmd=tmp;
+                        end
+                        
+                    else
+                        for i = 1:length(self.qregName)
+                            if (strcmp(tmp{2},self.qregName{i}))
+                                for j = 1:length(self.qregName)
+                                    if (strcmp(tmp{4},self.qregName{j}))
+                                        if ( str2double(self.noOfQreg{i}) <= str2double(self.noOfQreg{j}))
+                                            tmp = strrep(cmd,',',' , ');
+                                            tmp = strsplit(tmp,{' ',','});
+                                            tmp = strtrim(tmp);
+                                            self.correctCmd = tmp;
+                                            break;
+                                        end
+                                    end
+                                end
+                            end
+                        end
+                    end
                 end
             elseif startsWith(cmd,'measure ')
-                disp('measure');
+                tmp = strrep(cmd,'->',' -> ');
+                tmp = strsplit(tmp,' ');
+                tmp = strtrim(tmp);
+                if strcmp(tmp{1},'measure') && strcmp(tmp{3},'->')
+                    if ( ~isempty(strfind(tmp{2},'[')) ) && ( ~isempty(strfind(tmp{2},']')) ) && ( ~isempty(strfind(tmp{4},'[')) ) && ( ~isempty(strfind(tmp{4},']')) )
+                        keep=0;
+                        qb = strrep(tmp{2},'[',' [ ');
+                        qb = strrep(qb,']',' ] ');
+                        qb = strsplit(qb,' ');
+                        qb = strtrim(qb);
+                        if strcmp(qb{2},'[') && strcmp(qb{4},']')
+                            for i=1:length(self.qregName)
+                                if (strcmp(qb{1},self.qregName{i}))
+                                    if ( ~isnan(str2double(qb{3})) )
+                                        keep = keep+1;
+                                    end
+                                end
+                            end
+                        end
+                        cb = strrep(tmp{4},'[',' [ ');
+                        cb = strrep(cb,']',' ] ');
+                        cb = strsplit(cb,' ');
+                        cb = strtrim(cb);
+                        if strcmp(cb{2},'[') && strcmp(cb{4},']')
+                            for i=1:length(self.cregName)
+                                if (strcmp(cb{1},self.cregName{i}))
+                                    if ( ~isnan(str2double(cb{3})) )
+                                        keep = keep+1;
+                                    end
+                                end
+                            end
+                        end
+                        if(keep==2)
+                            tmp = strrep(cmd,'->',' -> ');
+                            tmp = strrep(tmp,'[',' [ ');
+                            tmp = strrep(tmp,']',' ] ');
+                            tmp = strsplit(tmp,{'->',' ','[',']'});
+                            tmp = strtrim(tmp);
+                            self.correctCmd=tmp;
+                        end
+                    else
+                        for i = 1:length(self.qregName)
+                            if (strcmp(tmp{2},self.qregName{i}))
+                                for j = 1:length(self.cregName)
+                                    if (strcmp(tmp{4},self.cregName{j}))
+                                        if ( str2double(self.noOfQreg{i}) <= str2double(self.noOfCreg{j}))
+                                            tmp = strrep(cmd,'->',' -> ');
+                                            tmp = strsplit(tmp,{' ','->'});
+                                            tmp = strtrim(tmp);
+                                            self.correctCmd = tmp;
+                                            break;
+                                        end
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
             elseif startsWith(cmd,'//')
-                correctCmd = cmd;
+                self.correctCmd{1} = cmd;
                 return;
             end
             
-            if isempty(correctCmd)
+            if isempty(self.correctCmd)
                 disp('Bad line of code skip...')
             end
             
@@ -107,16 +230,39 @@ classdef Transpiler
         function writeFile(~,textToWirte, outFileName)
             fileID = fopen(outFileName,'w');
             for k=1:length(textToWirte)
-                fprintf(fileID,'%s\n',textToWirte{k});
+                   fprintf(fileID,'%s\n',textToWirte{k});
             end
         end
         
-        function translate(self,codes)
+        function self = translate(self,codes)
             for k=1:length(codes)
-                correctCmd = checkQASMSyntax(code);
-                if isempty(correctCmd)
-                    
-                end
+                self = self.checkQASMSyntax(codes{k});
+                 if ~isempty(self.correctCmd)
+                     tmp = self.correctCmd;
+                      if strcmp(tmp{1},'OPENQASM')
+                          self.result{k} = "import ExeQu.CircuitComposer.*;"+newline+"import ExeQu.Gates.*;";
+                      elseif strcmp(tmp{1},'include')
+                          self.result{k} = strcat('%',tmp{1},' ',tmp{2});
+                      elseif startsWith(tmp{1},'//')
+                          self.result{k} = strrep(tmp{1},'//','%');
+                      elseif strcmp(tmp{1},'cx')
+                          if(length(tmp)==3)
+                              self.result{k} = '%cnot';
+                          else
+                              self.result{k} = strcat('circuit.cnot(',tmp{3},',',tmp{5},');');
+                          end
+                      elseif strcmp(tmp{1},'measure')   
+                          if(length(tmp)==3)
+                              self.result{k} = '%measure';
+                          else
+                              self.result{k} = strcat('circuit.measure(',tmp{3},',',tmp{5},');');
+                          end
+                      elseif strcmp(tmp{1},'qreg')
+                          self.result{k} = '%qreg';
+                      elseif strcmp(tmp{1},'creg')
+                          self.result{k} = '%creg';
+                      end
+                 end
             end
         end
         
--- example.m
+++
@@ -1,3 +0,0 @@
-import ExeQu.Transpiler.*;
-convertor = Transpiler();
-convertor.convert('QASMTest.qinc', 'newFile.m');
