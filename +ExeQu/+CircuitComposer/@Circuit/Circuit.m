59f79b75a415b03f99253fcc1aafc44716899492
roy <roy@localhost>
2020-04-29 20:39:10 +0700

    add barrier

--- +ExeQu/+CircuitComposer/@Circuit/Circuit.m
+++ +ExeQu/+CircuitComposer/@Circuit/Circuit.m
@@ -28,6 +28,7 @@ classdef Circuit < handle
 %           and wrap them in 'operation' struct before adding
 %           them to the circuit's 'operationQueue'
         add(self, operation);
+        barrier(self, varargin);
         x(self, target);
         y(self, target);
         z(self, target);
--- sandbox.m
+++ sandbox.m
@@ -2,6 +2,7 @@ import ExeQu.CircuitComposer.*;
 
 qc = Circuit(4, 3);
 qc.h(3);
+qc.barrier();
 qc.cnot(3,1);
 qc.cnot(3,2);
 
@@ -9,6 +10,7 @@ qc.measure(1, 1);
 qc.measure(2, 2);
 
 qc.draw();
+% celldisp(qc.peekOperations())
 result = qc.execute(1000);
 result.getCount();
 result.plotHistogram();
